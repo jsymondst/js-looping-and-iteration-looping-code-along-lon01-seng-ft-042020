0c4e765008b1b474130bb58acad80a21c47e2cc7
jsymondst <jammiesymondstayler@gmail.com>
2020-06-02 18:32:08 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function countDown(){
+    let counter = 10;
+    while (counter >=0){
+        console.log(counter);
+        counter--;
+    }
+}
+
+function writeCards(arr){
+    const result = []
+    for (let i = 0; i < arr.length; i++ ){
+        result.push(`Thank you, ${arr[i]}, for the wonderful surprise gift!`);
+    }
+    return result
+}
